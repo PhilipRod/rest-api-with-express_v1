09c62499cfe1fd827632ca483e29bcb9fe56693f
philipROd <philiprodriguez1001@gmail.com>
2022-03-02 13:00:57 +0000

    updated

--- routes.js
+++ routes.js
@@ -73,6 +73,7 @@ router.post('/courses',authenticateUser,asyncHandler(async (req,res)=>{
         if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
             const errors = error.errors.map(err=>err.message)
             res.status(400).json({errors})
+            
         }else{
             throw error
         }
